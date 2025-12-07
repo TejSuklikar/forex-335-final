35f4715e0e6d96ef5440edb97395c077c5e8e9f2
TejSuklikar <anitej@suklikar.org>
2025-12-07 15:59:37 -0500

    Fixed capitlization error

--- routes/news.js
+++ routes/news.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const axios = require('axios');
-const Search = require('../models/Search');
+const Search = require('../models/search');
 
 router.get('/', async (req, res) => {
   try {
